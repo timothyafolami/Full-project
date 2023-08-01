7588ffd9e975f774e40fb45462e8251df4d094a6
Timothy Afolami <109224656+timothyafolami@users.noreply.github.com>
2023-08-01 10:29:47 +0100

    Update fault_detector_app.py

--- fault_detector_app.py
+++ fault_detector_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import joblib
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
