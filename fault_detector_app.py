741783dc43b09e90a8b7f3322b23d89ec10aa8bd
Timothy Afolami <109224656+timothyafolami@users.noreply.github.com>
2023-08-01 10:21:09 +0100

    Update fault_detector_app.py

--- fault_detector_app.py
+++ fault_detector_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import joblib
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
