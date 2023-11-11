b9be06dd0b3b80a9ec7af2fe0d2a03bd6b464942
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2023-11-11 21:40:01 +0100

    removed unnecessary libs

--- pages/1_🏫_schoolbot.py
+++ pages/1_🏫_schoolbot.py
@@ -2,8 +2,6 @@ import streamlit as st
 from streamlit_chat import message
 from utils import get_initial_message, get_chatgpt_response, update_chat
 import os
-from dotenv import load_dotenv
-load_dotenv()
 import openai
 
 openai.api_key = st.secrets.OPENAI_API_KEY
