cf1ee1012219c6507a187256cc018f88b2b42474
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2023-11-12 12:44:54 +0100

    chat completion update

--- pages/3_🏫_schoolbot.py
+++ pages/3_🏫_schoolbot.py
@@ -31,7 +31,7 @@ if prompt := st.chat_input(" "):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ''
-        for response in openai.ChatCompletion.create(
+        for response in openai.chat.completion.create(
             model=st.session_state["openai_model"],
             messages=[
                 {"role": m["role"], "content": m["content"]}
