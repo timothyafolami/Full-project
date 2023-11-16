c5976a812bff80cc9a03527bd1b17b9d73fcc224
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2023-11-16 09:40:24 +0100

    output

--- pages/1_💻_laptop_scan.py
+++ pages/1_💻_laptop_scan.py
@@ -67,7 +67,7 @@ with st.expander("Laptop Fault Detection"):
             result_df['Prediction_category'] = result_df['Prediction'].apply(lambda x: "Faulty" if x == 1 else "Normal")
             output = result_df[result_df['Prediction']==1]
 
-            st.dataframe(result_df)
+            st.dataframe(output)
     else:
         st.write("No data available for analysis.")
 
